3c1e60604aa4736458b14399a92bffbeaa5834e6
VsPot07 <cocoacake84@gmail.com>
2023-03-02 12:01:12 +0900

    03/02
    시네마신 임포트, 잔 셋팅 수정

--- Assets/Scripts/TextUV.cs
+++ Assets/Scripts/TextUV.cs
@@ -14,21 +14,26 @@ public class TextUV : MonoBehaviour
     {
         Mesh mesh = new Mesh();
 
-        Vector3[] vertices = new Vector3[3];
-        Vector2[] uv = new Vector2[3];
-        int[] triangles = new int[3];
+        Vector3[] vertices = new Vector3[4];
+        Vector2[] uv = new Vector2[4];
+        int[] triangles = new int[6];
 
         vertices[0] = new Vector3(0, 0);
         vertices[1] = new Vector3(0, 5);
         vertices[2] = new Vector3(5, 5);
+        vertices[3] = new Vector3(5, 0);
 
         triangles[0] = 0;
         triangles[1] = 1;
         triangles[2] = 2;
+        triangles[3] = 0;
+        triangles[4] = 2;
+        triangles[5] = 3;
 
         uv[0] = new Vector2(0, 0);
         uv[1] = new Vector2(0, 1);
         uv[2] = new Vector2(1, 1);
+        uv[3] = new Vector2(1, 0);
 
         mesh.vertices = vertices;
         mesh.triangles = triangles;
