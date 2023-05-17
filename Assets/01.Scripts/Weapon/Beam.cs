cbb09f297cae6743a746c28fad7e5b5e098419d2
VsPot07 <cocoacake84@gmail.com>
2023-05-17 15:21:52 +0900

    0517

---
+++ Assets/01.Scripts/Weapon/Beam.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class Beam : PoolableMono
+{
+    private VisualEffect _beamMuzzle;
+    private VisualEffect _beamFlare;
+
+    private LineRenderer _lineRenderer;
+    private Light _beamLight;
+
+    [SerializeField] private float _beamLength = 10f;
+    public LayerMask _whatIsEnemy;
+
+    [SerializeField] private float _beamTime = 0.6f;
+    [SerializeField] private int _beamDamage = 5;
+
+    private void Awake()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _beamMuzzle = transform.Find("BeamMuzzle").GetComponent<VisualEffect>();
+        _beamLight = transform.Find("BeamMuzzle/BeamLight").GetComponent<Light>();
+        _beamFlare = transform.Find("BeamFlare").GetComponent<VisualEffect>();
+
+        _lineRenderer.enabled = false;
+        _beamLight.enabled = false;
+        _beamMuzzle.Stop();
+        _beamFlare.Stop();
+
+        Init();
+    }
+
+    public override void Init()
+    {
+        _lineRenderer.enabled = false;
+        _beamLight.enabled = false;
+        _beamMuzzle.Stop();
+        _beamFlare.Stop();
+    }
+
+    public void PreCharging()
+    {
+        _beamMuzzle.Play();
+        _beamLight.enabled = true;
+    }
+
+    public void FireBeam(int damage, Vector3 targetDir)
+    {
+        float r = _lineRenderer.startWidth;
+        RaycastHit hit;
+        bool isHit = Physics.SphereCast(transform.position, r, targetDir.normalized, out hit, _beamLength);
+        _lineRenderer.enabled = true;
+        _lineRenderer.SetPosition(0, transform.position);
+        if(isHit)
+        {
+            _lineRenderer.SetPosition(1, hit.point);
+            _beamFlare.transform.position = hit.point;
+
+            if(hit.collider.TryGetComponent<IDamageable>(out IDamageable health))
+            {
+                health.OnDamage(damage, hit.point, hit.normal);
+            }
+        }
+        else
+        {
+            Vector3 endpos = transform.position + targetDir * _beamLength;
+            _lineRenderer.SetPosition(1, endpos);
+            _beamFlare.transform.position = endpos;
+        }
+
+        _beamFlare.Play();
+        StartCoroutine(DelayStop());
+    }
+    
+    public void StopBeam()
+    {
+
+    }
+
+    private IEnumerator DelayStop()
+    {
+
+    }
+
+    private IEnumerator StopSequence()
+    {
+
+    }
+}
